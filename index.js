2b3e73d4ce4b0255a24a6eeea7d034d446215270
Justin Daniel <justinddaniel@gmail.com>
2017-08-25 18:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return ('There is nobody waiting to be served!') }
-  else {return (`Currently serving ${katzDeliLine.shift}.`)
+  else {return (`Currently serving ${katzDeliLine.shift()}.`)
   }
 }
