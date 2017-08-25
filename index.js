ecb96034ff605f8d1cdfb2cc1aa5d96d8ff749d0
Justin Daniel <justinddaniel@gmail.com>
2017-08-25 17:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
+function nowServing() { 
+  if (katzDeliLine.length === 0) {
+    return ('There is nobody waiting to be served!') }
+  else {console.log(katzDeliLine[0]); 
+  katzDeliLine.shift();
+  return katzDeliLine }
+}
