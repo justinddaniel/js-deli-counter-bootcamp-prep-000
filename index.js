d7488a2fc4147903bf0b291b9e83de11baf26aa0
Justin Daniel <justinddaniel@gmail.com>
2017-08-25 18:16:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nowServing(katzDeliLine) {
 }
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return ('There is currently empty.') }
+    return ('The line is currently empty.') }
     else {return (`The line is currently: ${katzDeliLine}`)}
 }
